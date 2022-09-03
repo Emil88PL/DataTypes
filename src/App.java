da50142701a3dcc9464b71f4f7d835e35d81765a
Emil <emil88pl@gmail.com>
2022-09-03 22:42:37 +0100

    init

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+        boolean bool = true;
+        byte bytes = 8;
+        char character = 'A';
+        short number = 16;
+        int anotherNumber = 32;
+        long aLongNumber = 64L;
+        float decimalNumber = 3.2f;
+        double anotherDecimalNumber = 6.4d;
+
+        
+       System.out.println();  // Just to have space from Blue text
+       System.out.println(bool); 
+       System.out.println(bytes);
+       System.out.println(character);
+       System.out.println(number);
+       System.out.println(anotherNumber);
+       System.out.println(aLongNumber);
+       System.out.println(anotherDecimalNumber);
+       System.out.println(decimalNumber);
+
+        
+     }
+}
---
+++ src/Type.java
@@ -0,0 +1,5 @@
+public class Type {
+    public boolean methodName() {
+        return true;
+    }
+}
